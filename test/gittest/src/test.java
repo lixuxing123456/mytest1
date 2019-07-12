7723a2be59aa7263ffb6420ef9bff468a24f9079
zhangsan <1628108333@qq.com>
2019-07-12 21:00:18 +0800

    test测试

---
+++ test/gittest/src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println(000);
+        System.out.println("0000");
+        System.out.println("sdsds");
+    }
+}
